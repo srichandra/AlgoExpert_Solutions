1b9ce8970116c06c3c76da0c59cfe2bd94848081
Srichandra Chilappagari <ch.srichandra@gmail.com>
2022-01-22 18:49:53 +0530

    Create insertionsort.py

---
+++ insertionsort.py
@@ -0,0 +1,10 @@
+def insertionSort(array):
+    for i in range(1,len(array)):
+      key=array[i]
+      j=i-1
+      while((j>=0) and array[j]>key):
+        array[j+1]=array[j]
+        j=j-1
+      array[j+1]=key
+	  return(array)
+    pass

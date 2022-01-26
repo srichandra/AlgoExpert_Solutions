4afad946a908ce1d80e9c8947e6836cf03ec7d2f
Srichandra Chilappagari <ch.srichandra@gmail.com>
2022-01-27 01:09:38 +0530

    Create treetraversal.py

---
+++ treetraversal.py
@@ -0,0 +1,25 @@
+def inOrderTraverse(tree, array):
+    if tree is not None:
+		inOrderTraverse(tree.left,array)
+		array.append(tree.value)
+		inOrderTraverse(tree.right,array)
+		return array		
+    pass
+
+
+def preOrderTraverse(tree, array):
+    if tree is not None:		
+		array.append(tree.value)
+		preOrderTraverse(tree.left,array)
+		preOrderTraverse(tree.right,array)
+		return array
+    pass
+
+
+def postOrderTraverse(tree, array):
+    if tree is not None:				
+		postOrderTraverse(tree.left,array)
+		postOrderTraverse(tree.right,array)
+		array.append(tree.value)
+		return array
+    pass
